ae8423d3b49cd59971b03524cb85ba06780066d2
FBurgeat <fburgeat@groupeseb.com>
2022-01-04 14:46:20 +0100

    feat: design Joker skeleton display

--- swade-helper/src/app/party/components/party-page/party-page.component.ts
+++ swade-helper/src/app/party/components/party-page/party-page.component.ts
@@ -5,9 +5,35 @@ import { PartyService } from 'src/app/services/party.service';
   selector: 'party-page',
   template: `
     <h1>{{ party?.name }}</h1>
+    <mat-card>
+      <mat-card-title>Personnages</mat-card-title>
+      <mat-card-content>
+        <mat-card-subtitle>Joker Joueurs</mat-card-subtitle>
+        <mat-card-subtitle>Joker PNJ</mat-card-subtitle>
+        <mat-card-subtitle>Extras</mat-card-subtitle>
+      </mat-card-content>
+      <mat-card-actions align="end">
+        <button mat-fab color="primary" aria-label="Créer nouveau Joker">
+          <mat-icon>add</mat-icon>
+        </button>
+      </mat-card-actions>
+    </mat-card>
+    <button mat-raised-button color="warn">
+      <mat-icon>sync</mat-icon>
+      Nouveau Round
+    </button>
   `,
-  styles: [
-  ]
+  styles: [`
+  :host {
+    flex: 1 0 auto;
+    padding: 1em;
+    display: flex;
+    flex-direction: column;
+    :not(:last-child) {
+      margin-bottom: 1em;
+    }
+  }
+  `]
 })
 export class PartyPageComponent implements OnInit {
   constructor(
