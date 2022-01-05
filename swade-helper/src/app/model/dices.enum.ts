545da5be0f81e142ccae7c37a6be17dd9ef346c6
FBurgeat <fburgeat@groupeseb.com>
2022-01-05 19:54:43 +0100

    feat: create extra form page

---
+++ swade-helper/src/app/model/dices.enum.ts
@@ -0,0 +1,13 @@
+enum Dices {
+  d4 = 4,
+  d6 = 6,
+  d8 = 8,
+  d10 = 10,
+  d12 = 12,
+  d12p1 = 13,
+  d12p2 = 14,
+  d12p3 = 15,
+  d12p4 = 16
+}
+
+export default Dices;
--- swade-helper/src/app/party/components/create-extra-page/create-extra-page.component.ts
+++ swade-helper/src/app/party/components/create-extra-page/create-extra-page.component.ts
@@ -1,20 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import Dices from 'src/app/model/dices.enum';
 
 @Component({
   selector: 'app-create-extra-page',
   template: `
-    <p>
-      create-extra-page works!
-    </p>
+  <mat-card>
+    <h1>Nouvel Extra</h1>
+    <form [formGroup]="extraForm" (ngSubmit)="submit()">
+      <mat-form-field>
+        <mat-label>Nom</mat-label>
+        <input formControlName="name" matInput placeholder="Sous-fifre Nazi" required>
+      </mat-form-field>
+    </form>
+  </mat-card>
   `,
   styles: [
   ]
 })
 export class CreateExtraPageComponent implements OnInit {
-
-  constructor() { }
+  public extraForm = this.formBuilder.group({
+    name: [''],
+    personality: [''],
+    parade: [0],
+    allure: [0],
+    resistance: [0],
+    stats: this.formBuilder.group({
+      agi: [Dices.d4],
+      ame: [Dices.d4],
+      for: [Dices.d4],
+      int: [Dices.d4],
+      vig: [Dices.d4]
+    }),
+    skills: this.formBuilder.array([])
+  })
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  addSkill () {
+    const skills = this.extraForm.get('skills') as FormArray;
+    skills.push(this.formBuilder.group({
+      dice: [Dices.d4],
+      name: ['']
+    }));
+  }
+
+  submit() {
+
   }
 
 }
